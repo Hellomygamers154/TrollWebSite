ac537a636e1a48ee3beb4b0829ea6557536ab119
Hellomygamers154 <115946221+Hellomygamers154@users.noreply.github.com>
2022-12-19 21:52:32 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,247 @@
+
+<!doctype html>
+<html>
+<head>
+  <title>e621</title>
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/manifest.json">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00549e">
+<meta name="theme-color" content="#00549e">
+<meta name="RATING" content="RTA-5042-1996-1400-1577-RTA" />
+<link rel="top" title="e621" href="/">
+<meta name="csrf-param" content="authenticity_token" />
+<meta name="csrf-token" content="KLEF8bzBtL083dODDefsUY_9JUgLnaISwD-I1UOJu3jAPeWPKmItrOM8naQCq_iuHf9-7jpCiJcVSSbS9Xv7JQ" />
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+
+<meta name="current-user-name" content="Anonymous">
+<meta name="current-user-id" content="">
+<meta name="current-user-can-approve-posts" content="false">
+<meta name="user-comment-threshold" content="-10">
+  <meta name="blacklisted-tags" content="[&quot;gore&quot;,&quot;scat&quot;,&quot;watersports&quot;,&quot;young -rating:s&quot;,&quot;loli&quot;,&quot;shota&quot;]">
+  <meta name="blacklist-users" content="false">
+<meta name="enable-js-navigation" content="true">
+<meta name="enable-auto-complete" content="true">
+<meta name="style-usernames" content="true">
+<meta name="last-forum-read-at" content="1959-12-31 19:00:00 -0500">
+
+<link rel="alternate" type="application/atom+xml" title="ATOM" href="/posts.atom" />
+
+<script src="/vendor/jquery-3.5.0.min.js" crossorigin="anonymous" nonce="PMC3uAJNTxXhVw3c7KJalA==" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ="></script>
+<link rel="stylesheet" crossorigin="anonymous" href="/vendor/fontawesome/css/all.min.css" nonce="true" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" />
+<link rel="stylesheet" href="/packs/css/753-3715d468.css" nonce="true" />
+<link rel="stylesheet" href="/packs/css/application-a9f0fb84.css" nonce="true" />
+<script src="/packs/js/753-5bfe3755d2f3d2e20b8b.js" nonce="PMC3uAJNTxXhVw3c7KJalA=="></script>
+<script src="/packs/js/application-777bcdac7f717220813f.js" nonce="PMC3uAJNTxXhVw3c7KJalA=="></script>
+
+<script type="application/ld+json">
+{
+    "@context": "http://schema.org",
+    "@type": "WebSite",
+    "url" : "https://e621.net/",
+    "potentialAction": [{
+        "@type": "SearchAction",
+        "target": "https://e621.net/posts?tags={search_term_string}",
+        "query-input": "required name=search_term_string"
+    }]
+}
+</script>
+<script type="application/ld+json">
+{
+    "@context": "http://schema.org",
+    "@type": "WebSite",
+    "name": "e621",
+    "alternateName": "e621",
+    "url" : "https://e621.net/"
+}
+</script>
+
+<style id="blacklisted-hider">
+  .post-preview, #image-container, #c-comments .post, .post-thumbnail {
+    visibility: hidden !important;
+  }
+</style>
+<noscript>
+  <style>
+    .post-preview, #image-container, #c-comments .post, .post-thumbnail {
+    visibility: visible !important;
+    }
+</style>
+</noscript>
+
+
+<meta name="enable-auto-complete" content="true">
+
+
+</head>
+<body lang="en" class="c-static a-home resp" data-controller="static" data-action="home" data-user-name="Anonymous" data-user-level="0" data-user-level-string="Anonymous" data-user-can-approve-posts="false" data-user-can-upload-free="false" data-user-per-page="75" data-user-is-contributor="false" data-user-is-former-staff="false" data-user-is-blocked="false" data-user-is-janitor="false" data-user-is-moderator="false" data-user-is-admin="false" data-user-is-anonymous="true" data-user-is-member="false" data-user-is-privileged="false" data-user-is-banned="false" data-user-is-approver="false" data-user-is-voter="false" data-user-is-verified="false">
+  <script nonce="PMC3uAJNTxXhVw3c7KJalA==">
+    //<![CDATA[
+(function() {
+  try {
+    var theme = localStorage.getItem('theme') || 'hexagon';
+    var extra = localStorage.getItem('theme-extra') || 'hexagon';
+    var nav = localStorage.getItem('theme-nav') || 'top';
+    var b = document.body;
+    b.setAttribute('data-th-main', theme);
+    b.setAttribute('data-th-extra', extra);
+    b.setAttribute('data-th-nav', nav);
+} catch(e) {
+}
+})();
+
+    //]]>
+</script>
+  <div id="page">
+    <style type="text/css">
+  body,div,h1,h2,h3,h4,h5,h6,p,ul,li,dd,dt {
+    font-family:verdana,sans-serif;
+    margin:0;
+    padding:0;
+}
+
+  body {
+    background-color: #012e56;
+    background-attachment:fixed;
+    background-position:50% 0%;
+    background-repeat:no-repeat;
+}
+
+  #page {
+    background: none !important;
+}
+
+  #links > a {
+    padding: 0.25rem 0.25rem;
+}
+  #links {
+    margin-bottom: 0.25rem;
+}
+
+  div#a-home {
+    text-align: center;
+    position:absolute;
+    top:28em;
+    left:0;
+    right:0;
+}
+
+  .mascotbox {
+    z-index: 1;
+    overflow: hidden;
+    position: relative;
+
+    background-repeat:no-repeat;
+    background-attachment:fixed;
+    background-position:50% 0;
+    margin: 10px auto;
+    padding: 2px 0;
+    width: 480px;
+    max-width: 98vw;
+    border-radius: 5px;
+    box-shadow: 0 0 5px #000;
+    text-shadow: 0 0 2px black, 0 0 6px black;
+}
+
+  .mascotbox:before {
+    z-index: -1;
+    content: "";
+    position: absolute;
+    width: 200vw;
+    height: 200vh;
+    top: -100px;
+    left: -100px;
+
+    background-image: inherit;
+    background-color: inherit;
+    background-repeat: inherit;
+    background-attachment: inherit;
+    background-position: inherit;
+    filter: blur(8px);
+}
+
+  #searchbox {
+    padding-bottom:5px;
+}
+
+  .news-excerpt {
+    padding: 5px 1em 0 1em;
+}
+
+  #searchbox3, #news-excerpt-box .previous-news-link {
+    font-size: 80%;
+}
+
+  @media only screen and (max-height: 600px) {
+    div#a-home {
+    top: 25vh;
+}
+}
+</style>
+
+<div id="c-static">
+  <div id="a-home">
+    <script nonce="PMC3uAJNTxXhVw3c7KJalA==">
+    //<![CDATA[
+      var mascots = {"9":{"id":9,"background_color":"#012e57","artist_url":"https://www.furaffinity.net/user/ratte","artist_name":"Ratte","background_url":"https://static1.e621.net/data/mascots/8adec88535da7b2d3fc825583e1b50db.png"},"14":{"id":14,"background_color":"#012e57","artist_url":"https://www.furaffinity.net/user/ratte","artist_name":"Ratte","background_url":"https://static1.e621.net/data/mascots/7500bdb80bdc353585896fa8a9aaabd3.png"},"13":{"id":13,"background_color":"#012e57","artist_url":"http://www.furaffinity.net/user/chizi","artist_name":"chizi","background_url":"https://static1.e621.net/data/mascots/53a014c46528923fc4baf2192af926bd.png"},"3":{"id":3,"background_color":"#012e56","artist_url":"http://nowhereincoming.net","artist_name":"darkdoomer","background_url":"https://static1.e621.net/data/mascots/09311de458e6b349759c9283895a0255.jpg"},"6":{"id":6,"background_color":"#012e57","artist_url":"http://www.horsecore.org","artist_name":"ECMajor","background_url":"https://static1.e621.net/data/mascots/adccc1706fc9e1a04dde79a3613c6da7.jpg"},"7":{"id":7,"background_color":"#012e57","artist_url":"https://www.furaffinity.net/user/evalion","artist_name":"evalion","background_url":"https://static1.e621.net/data/mascots/77985db3b532b3ee9ae01d245ae04e09.jpg"},"2":{"id":2,"background_color":"#012e56","artist_url":"https://www.furaffinity.net/user/keishinkae","artist_name":"Keishinkae","background_url":"https://static1.e621.net/data/mascots/913a8fd0240b14bfbb63d6a9cfc3faf2.jpg"},"1":{"id":1,"background_color":"#012e56","artist_url":"https://www.furaffinity.net/user/keishinkae","artist_name":"Keishinkae","background_url":"https://static1.e621.net/data/mascots/f34b76aa4673cd2ae91e91099914b290.jpg"},"8":{"id":8,"background_color":"#012e57","artist_url":"https://www.furaffinity.net/user/ratte","artist_name":"Ratte","background_url":"https://static1.e621.net/data/mascots/aaf516a2caabe2e837ab57c754e0cb25.png"},"11":{"id":11,"background_color":"#012e57","artist_url":"https://www.furaffinity.net/user/ratte","artist_name":"Ratte","background_url":"https://static1.e621.net/data/mascots/f1cb84565a8bcab0cfd486de04a4b2ca.png"},"5":{"id":5,"background_color":"#012e56","artist_url":"https://www.furaffinity.net/user/wiredhooves","artist_name":"wiredhooves","background_url":"https://static1.e621.net/data/mascots/b426389c44ade604820e2295bf77f464.jpg"},"4":{"id":4,"background_color":"#002d55","artist_url":"http://www.furaffinity.net/user/chizi","artist_name":"chizi","background_url":"https://static1.e621.net/data/mascots/e67f0b91246775999a244d28c2d3d158.jpg"},"12":{"id":12,"background_color":"#012e57","artist_url":"http://www.furaffinity.net/user/chizi","artist_name":"chizi","background_url":"https://static1.e621.net/data/mascots/7f2adcaccb3f4bbff348470806e40866.png"}};
+
+    //]]>
+</script>
+    <div id="searchbox" class='mascotbox'>
+      <div id="static-index">
+        <h1 style="font-size: 4em;"><a href="/">e621</a></h1>
+        <div id="links">
+            <a title="Login or sign up" href="/session/new">Login/Signup</a>
+          <a title="A paginated list of every post" href="/posts">Posts</a>
+          <a title="A paginated list of every comment" href="/comments">Comments</a>
+          <a title="A paginated list of every tag" href="/tags">Tags</a>
+          <a title="Wiki" href="/wiki_pages?title=help%3Ahome">Wiki</a>
+          <a title="Forum" href="/forum_topics">Forum</a>
+          <a title="A site map" href="/static/site_map">&raquo;</a>
+        </div>
+        <div>
+          <form action="/posts" accept-charset="UTF-8" method="get">
+            <div>
+              <input type="text" name="tags" id="tags" value="" size="30" autofocus="autofocus" data-autocomplete="tag-query" /><br>
+              <input type="submit" value="Search">
+              <input type="button" value="Change Mascot" id="change-mascot">
+            </div>
+</form>        </div>
+      </div>
+      <div id='mascot_artist'></div>
+    </div>
+
+    <div id='searchbox2' class='mascotbox'>
+        <img src="/images/counter/3.png" />
+        <img src="/images/counter/2.png" />
+        <img src="/images/counter/4.png" />
+        <img src="/images/counter/7.png" />
+        <img src="/images/counter/4.png" />
+        <img src="/images/counter/1.png" />
+        <img src="/images/counter/7.png" />
+    </div>
+
+      <div id="news-excerpt-box" class="mascotbox">
+        <div class="news-excerpt dtext-container">If you are an artist uploading your own work, please tag yourself. Otherwise, people cannot easily find the rest of your work. It helps you, it helps us.</div>
+        <div class="previous-news-link"><a href="/news_updates">Click here for previous news</a></div>
+      </div>
+
+    <div id='searchbox3' class='mascotbox'>
+      <p>
+        Serving 3,247,417 posts<br/>
+        <a title="Takedown Information" href="/static/takedown">Takedown Policy and Process</a>
+        |
+        <a title="Contact Us" href="/static/contact">Contact Us</a> |
+        <a title="Terms of Service" href="/static/terms_of_service">Terms of Service</a> |
+        <a title="Privacy Policy" href="/static/privacy">Privacy</a>
+      </p>
+    </div>
+
+  </div>
+</div>
+
+
+  </div>
+</body></html>
